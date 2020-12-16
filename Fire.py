acb37285783314d5a2e7425768e2938a102cb46f
Lindsey <15371952+LVParkinson@users.noreply.github.com>
2020-12-16 19:09:45 +0100

    simplified 2 fxns in pipeline

--- Fire.py
+++ Fire.py
@@ -57,17 +57,16 @@ def convert_to_csv(filepath, csv_name):
     
     df.to_csv(csv_name, index = False) 
     
-    print(df)
+    print(df.head())
     print("~~~~check source folder for csv~~~~")
+    return df
     
     
-    
-def find_urls(pathtocsv):
+def find_urls(df):
     """
     Extract urls from comments. 
     Only works with full urls containing "http"
     """
-    df = pd.read_csv(pathtocsv)
     
     links=[]
     for comment in df['comment']:
@@ -75,13 +74,12 @@ def find_urls(pathtocsv):
     return links
     
 
-def comments_by_author(pathtocsv):
+def comments_by_author(df):
     """
     Count the number of comments made by each author
     """
-    df = pd.read_csv(pathtocsv)
-    print( df.groupby(df['author'])['author'].count().sort_values(ascending=False))
-
+    authorcomments = df.groupby(df['author'])['author'].count().sort_values(ascending=False)
+    return authorcomments
 
 
 def comments_over_time(pathtocsv):
@@ -156,28 +154,13 @@ def comment_network(pathtocsv):
     print("~~~~check working directory for Zoom_network.png~~~~")
 
     
-def csv_pipeline(pathtocsv):    
-    df = pd.read_csv(pathtocsv)
-    
-    def find_urls(pathtocsv):
-        """
-        Extract urls from comments. 
-        Only works with full urls containing "http"
-        """
-        links=[]
-        for comment in df['comment']:
-            links.extend(re.findall(r'(https?://[^\s]+)', comment))
-        return links
-    find_urls(pathtocsv)
-    
-    def comments_by_author(pathtocsv):
-        """
-        Count the number of comments made by each author
-        """
-        print( df.groupby(df['author'])['author'].count().sort_values(ascending=False))
-    comments_by_author(pathtocsv)
-        
-    def comments_over_time(pathtocsv):
+
+
+def start_to_finish(filepath, csv_name):    
+    df = convert_to_csv(filepath, csv_name)
+
+
+    def comments_over_time(df):
         """
         Count the number of comments over time. Return histogram.
         tinterval: Optional. the time interval, in minutes, to count comments
@@ -217,11 +200,12 @@ def csv_pipeline(pathtocsv):
         ax.set_ylabel("Number of messages")
         ax.set_title("number of messages in 10-min intervals")
         plt.savefig("Zoom_histogram.png")
-
+        print("  ")
         print("~~~~check working directory for Zoom_histogram.png~~~~")
-    comments_over_time(pathtocsv)
-    
-    def comment_network(pathtocsv):
+        print("  ")
+    comments_over_time(df)
+
+    def comment_network(df):
         """
         Create a network graph of who responds to comments
         Lines are drawn between the author of a comment and the next person to comment
@@ -239,176 +223,11 @@ def csv_pipeline(pathtocsv):
         nx.draw_shell(G, with_labels=True,font_weight='bold')
 
         plt.savefig("Zoom_network.png", bbox_inches="tight")
-
+        print("  ")
         print("~~~~check working directory for Zoom_network.png~~~~")
-    comment_network(pathtocsv)
-    
-    print("Pipeline complete. Check directory")
-
-def start_to_finish(filepath, csv_name):    
-    
-    def convert_to_csv(filepath, csv_name):
-        file = open(filepath, mode='r',encoding="utf8")
-
-        time = []
-        author =[]
-        comment =[]
-
-        regex_time = r'\d{2}:\d{2}:\d{2}'
-        regex_author = r'\bFrom \s(.*?:)' #this could be improved to avoid the space and colon at the end
-        regex_comment = r'(?:\: )(.*$)'
-
-        for line in file:
-        # Empty lines will be ignored
-            if line.strip(): 
-            #seperate line to get a view of comments
-                #print(line)
-            # use .extend() instead of .append() to avoid making a list of single-item lists
-                time.extend(re.findall(regex_time, line))
-                author.extend(re.findall(regex_author, line))
-                comment.extend(re.findall(regex_comment, line))
         print("  ")
-        print("last comment:")
-        print(line)
+    comment_network(df)
 
-        s1=pd.Series(time,name='time')
-        s2=pd.Series(author,name='author')
-        s3=pd.Series(comment ,name='comment')
-
-        df = pd.concat([s1,s2,s3], axis=1)
-
-        df['author'] = df['author'].str[:-2]
-        df = df[~df['author'].str.contains("Privately")].reset_index(drop=True)
-
-        df.to_csv(csv_name, index = False) 
-        print("~~~~check source folder for csv~~~~")
-        print("  ")
-        print("Head of dataframe:")
-        print(df.head())
-
-        def find_urls(df):
-            """
-            Extract urls from comments. 
-            Only works with full urls containing "http"
-            """
-            links=[]
-            for comment in df['comment']:
-                links.extend(re.findall(r'(https?://[^\s]+)', comment))
-            
-            print("  ")
-            print("~~~~check working directory for Zoom_urls.txt~~~~")
-            print("  ")
-            for link in links: 
-                print(link)
-            #pdf = fpdf.FPDF(format = 'letter')
-            #pdf.add_page()
-            #pdf.set_font("Arial", size=12)
-
-            #for link in str(links):
-            #pdf.write(str(links))
-            #pdf.output("linktest.pdf")
-            
-            urlFile=open('Zoom_urls.txt','w')
-            links=map(lambda x:x+'\n', links)
-            urlFile.writelines(links)
-            urlFile.close()
-            
-            
-        find_urls(df)
-
-        def comments_by_author(df):
-            """
-            Count the number of comments made by each author
-            """     
-            #group and count comments by author
-            authorcomments = df.groupby(df['author'])['author'].count().sort_values(ascending=False)
-            
-            
-            #turn into list
-            #authorcount = []
-            #for author in authorcomments:
-            #    authorcount.extend(author)
-                
-            authorFile=open('Author_count.txt','w')
-            #authorcount=map(lambda x:x+'\n', authorcount)
-            authorFile.writelines(str(authorcomments))
-            authorFile.close()
-            
-            print("  ")
-            print("~~~~check working directory for Author_count.txt~~~~")
-            print("  ")
-            print(authorcomments)
-            
-        comments_by_author(df)
-
-        def comments_over_time(df):
-            """
-            Count the number of comments over time. Return histogram.
-            tinterval: Optional. the time interval, in minutes, to count comments
-            yticks: Optional. Y-axis tick mark interval
-            """
-            tinterval = 10 
-            yticks = 2
-
-            #convert string to time
-            df['time'] = pd.to_datetime(df['time'],format= '%H:%M:%S' ).dt.time
-
-            # get a list of the times
-            times = [t.hour + t.minute/60. for t in df['time']]
-
-            # set the time interval required (in minutes)
-            tinterval = tinterval
-
-            # find the lower and upper bin edges (on an integer number of 10 mins past the hour)
-            lowbin = np.min(times) - np.fmod(np.min(times)-np.floor(np.min(times)), tinterval/60.)
-            highbin = np.max(times) - np.fmod(np.max(times)-np.ceil(np.max(times)), tinterval/60.)
-            bins = np.arange(lowbin, highbin, tinterval/60.)  # set the bin edges
-
-
-            # create the histogram
-            plt.hist(times, bins=bins)
-            ax = plt.gca()  # get the current plot axes
-            ax.set_xticks(bins)  # set the position of the ticks to the histogram bin edges
-            # create new labels in hh:mm format (in twelve hour clock)
-            newlabels = []
-            for edge in bins:
-                h, m = divmod(edge%12, 1)  # get hours and minutes (in 12 hour clock)
-                newlabels.append('{0:01d}:{1:02d}'.format(int(h), int(m*60)))  # create the new label
-
-            ax.set_xticklabels(newlabels, rotation = 60)  # set the new labels
-            start, end = ax.get_ylim()
-            ax.yaxis.set_ticks(np.arange(start, end, yticks))
-            ax.set_ylabel("Number of messages")
-            ax.set_title("number of messages in 10-min intervals")
-            plt.savefig("Zoom_histogram.png")
-            print("  ")
-            print("~~~~check working directory for Zoom_histogram.png~~~~")
-            print("  ")
-        comments_over_time(df)
-
-        def comment_network(df):
-            """
-            Create a network graph of who responds to comments
-            Lines are drawn between the author of a comment and the next person to comment
-
-            """
-            dfchatters = df[['author']]
-            dfchatters['responder'] = df['author'].shift(periods=-1)
-            dfchatters = dfchatters.dropna()
-
-            G = nx.DiGraph()
-
-            G = nx.from_pandas_edgelist(dfchatters, 'author', 'responder')
-
-            figure(figsize=(23, 15))
-            nx.draw_shell(G, with_labels=True,font_weight='bold')
-
-            plt.savefig("Zoom_network.png", bbox_inches="tight")
-            print("  ")
-            print("~~~~check working directory for Zoom_network.png~~~~")
-            print("  ")
-        comment_network(df)
-    
     #Create pdf
     pdf = fpdf.FPDF(format = 'letter')
     #Page 1
@@ -416,16 +235,16 @@ def start_to_finish(filepath, csv_name):
     pdf.set_font("Arial", size = 15)
     pdf.cell(200, 10, txt = "Zoom Chat - Basic Analytics",  
          ln = 1, align = 'C') 
-    a = open("Author_count.txt", "r", encoding ="utf8")
-    for line in a: 
-        if line.strip():
-            pdf.cell(200, 10, txt = line)
+    author_list = comments_by_author(df)
+
+    for author, count in author_list.items():
+        pdf.cell(200, 5, txt = f"{author} - {count}", ln = 1)
+
     #Page 2
     pdf.add_page()
-    u = open("Zoom_urls.txt", "r", encoding ="utf8")
-    for line in u: 
-        if line.strip():
-            pdf.cell(200, 10, txt = line)
+    url_list = find_urls(df)
+    for url in url_list: 
+        pdf.cell(200, 5, txt = url, ln = 1)
     #Page 3
     pdf.add_page()
     pdf.cell(200, 10, txt = "Zoom Chat Histogram",  
@@ -436,13 +255,14 @@ def start_to_finish(filepath, csv_name):
     pdf.cell(200, 10, txt = "Zoom Chat Network",  
          ln = 1, align = 'C')
     pdf.image("Zoom_network.png", w=200, h=200)
-    
+
     pdf.output("test.pdf")
-    
-    convert_to_csv(filepath, csv_name)
-    print("  ")
-    print("~~~~Pipeline complete. Check working directory~~~~")        
-    print("  ")
+
+
+
+
+    print("\n~~~~Pipeline complete. Check working directory~~~~\n")        
+
     
 if __name__ == '__main__':
     fire.Fire({
@@ -451,6 +271,5 @@ if __name__ == '__main__':
       'comments_by_author': comments_by_author,
       'comments_over_time': comments_over_time,
       'comment_network': comment_network,
-      'csv_pipeline':csv_pipeline,
       'start_to_finish': start_to_finish
   })
