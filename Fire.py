7aa9c867e9adb09698bfa0d8977679cde215b24c
Lindsey <15371952+LVParkinson@users.noreply.github.com>
2020-12-17 14:38:18 +0100

    removed copied functions

--- Fire.py
+++ Fire.py
@@ -6,7 +6,7 @@ Exports csv with three columns: time, author, and comment
 Runs a few functions for basic analysis
 
 Author: Lindsey Viann Parkinson
-Last Updated: December 15, 2020
+Last Updated: December 16, 2020
 
 """
 # Import modules and packages
@@ -38,13 +38,10 @@ def convert_to_csv(filepath, csv_name):
     for line in file:
     # Empty lines will be ignored
         if line.strip(): 
-        #seperate line to get a view of comments
-            #print(line)
         # use .extend() instead of .append() to avoid making a list of single-item lists
             time.extend(re.findall(regex_time, line))
             author.extend(re.findall(regex_author, line))
             comment.extend(re.findall(regex_comment, line))
-        print(line)
     
     s1=pd.Series(time,name='time')
     s2=pd.Series(author,name='author')
@@ -58,7 +55,7 @@ def convert_to_csv(filepath, csv_name):
     df.to_csv(csv_name, index = False) 
     
     print(df.head())
-    print("~~~~check source folder for csv~~~~")
+    print("\n~~~~check source folder for csv~~~~\n")
     return df
     
     
@@ -82,32 +79,26 @@ def comments_by_author(df):
     return authorcomments
 
 
-def comments_over_time(pathtocsv):
+def comments_over_time(df):
     """
     Count the number of comments over time. Return histogram.
     tinterval: Optional. the time interval, in minutes, to count comments
     yticks: Optional. Y-axis tick mark interval
-    """
-    tinterval = 10 
-    yticks = 2
-    
-    df = pd.read_csv(pathtocsv)
-    
+    """  
     #convert string to time
     df['time'] = pd.to_datetime(df['time'],format= '%H:%M:%S' ).dt.time
 
     # get a list of the times
     times = [t.hour + t.minute/60. for t in df['time']]
 
-    # set the time interval required (in minutes)
-    tinterval = tinterval
+    # set the x-axis time interval required (in minutes)
+    tinterval = 10
 
     # find the lower and upper bin edges (on an integer number of 10 mins past the hour)
     lowbin = np.min(times) - np.fmod(np.min(times)-np.floor(np.min(times)), tinterval/60.)
     highbin = np.max(times) - np.fmod(np.max(times)-np.ceil(np.max(times)), tinterval/60.)
-    bins = np.arange(lowbin, highbin, tinterval/60.)  # set the bin edges
-    
-    
+    bins = np.arange(lowbin, highbin, tinterval/60.)  # set the bin edges    
+     
     # create the histogram
     plt.hist(times, bins=bins)
     ax = plt.gca()  # get the current plot axes
@@ -121,23 +112,20 @@ def comments_over_time(pathtocsv):
 
     ax.set_xticklabels(newlabels, rotation = 60)  # set the new labels
     start, end = ax.get_ylim()
-    ax.yaxis.set_ticks(np.arange(start, end, yticks))
+    ax.yaxis.set_ticks(np.arange(start, end, 2))
     ax.set_ylabel("Number of messages")
     ax.set_title("number of messages in 10-min intervals")
     plt.savefig("Zoom_histogram.png")
     
-    print("~~~~check working directory for Zoom_histogram.png~~~~")
-
+    print("\n~~~~check working directory for Zoom_histogram.png~~~~\n")
     
     
-def comment_network(pathtocsv):
+    
+def comment_network(df):
     """
     Create a network graph of who responds to comments
     Lines are drawn between the author of a comment and the next person to comment
-    
-    """
-    df = pd.read_csv(pathtocsv)
-    
+    """    
     dfchatters = df[['author']]
     dfchatters['responder'] = df['author'].shift(periods=-1)
     dfchatters = dfchatters.dropna()
@@ -151,117 +139,70 @@ def comment_network(pathtocsv):
 
     plt.savefig("Zoom_network.png", bbox_inches="tight")
     
-    print("~~~~check working directory for Zoom_network.png~~~~")
+    print("\n~~~~check working directory for Zoom_network.png~~~~\n")
 
     
-
-
+def individual_function(filepath, csv_name, function_name):
+    """
+    Call only one function of Zoom Chat Analytics
+    filepath: path to .txt file 
+    csv_name: what the output csv will be called (include .csv !)
+    function_name: which individual function you'd like to run
+    """
+    df = convert_to_csv(filepath, csv_name)
+    function = function_name + '(df)'
+    function
+    
 def start_to_finish(filepath, csv_name):    
     df = convert_to_csv(filepath, csv_name)
 
-
-    def comments_over_time(df):
-        """
-        Count the number of comments over time. Return histogram.
-        tinterval: Optional. the time interval, in minutes, to count comments
-        yticks: Optional. Y-axis tick mark interval
-        """
-        tinterval = 10 
-        yticks = 2
-
-        #convert string to time
-        df['time'] = pd.to_datetime(df['time'],format= '%H:%M:%S' ).dt.time
-
-        # get a list of the times
-        times = [t.hour + t.minute/60. for t in df['time']]
-
-        # set the time interval required (in minutes)
-        tinterval = tinterval
-
-        # find the lower and upper bin edges (on an integer number of 10 mins past the hour)
-        lowbin = np.min(times) - np.fmod(np.min(times)-np.floor(np.min(times)), tinterval/60.)
-        highbin = np.max(times) - np.fmod(np.max(times)-np.ceil(np.max(times)), tinterval/60.)
-        bins = np.arange(lowbin, highbin, tinterval/60.)  # set the bin edges
-
-
-        # create the histogram
-        plt.hist(times, bins=bins)
-        ax = plt.gca()  # get the current plot axes
-        ax.set_xticks(bins)  # set the position of the ticks to the histogram bin edges
-        # create new labels in hh:mm format (in twelve hour clock)
-        newlabels = []
-        for edge in bins:
-            h, m = divmod(edge%12, 1)  # get hours and minutes (in 12 hour clock)
-            newlabels.append('{0:01d}:{1:02d}'.format(int(h), int(m*60)))  # create the new label
-
-        ax.set_xticklabels(newlabels, rotation = 60)  # set the new labels
-        start, end = ax.get_ylim()
-        ax.yaxis.set_ticks(np.arange(start, end, yticks))
-        ax.set_ylabel("Number of messages")
-        ax.set_title("number of messages in 10-min intervals")
-        plt.savefig("Zoom_histogram.png")
-        print("  ")
-        print("~~~~check working directory for Zoom_histogram.png~~~~")
-        print("  ")
-    comments_over_time(df)
-
-    def comment_network(df):
-        """
-        Create a network graph of who responds to comments
-        Lines are drawn between the author of a comment and the next person to comment
-
-        """
-        dfchatters = df[['author']]
-        dfchatters['responder'] = df['author'].shift(periods=-1)
-        dfchatters = dfchatters.dropna()
-
-        G = nx.DiGraph()
-
-        G = nx.from_pandas_edgelist(dfchatters, 'author', 'responder')
-
-        figure(figsize=(23, 15))
-        nx.draw_shell(G, with_labels=True,font_weight='bold')
-
-        plt.savefig("Zoom_network.png", bbox_inches="tight")
-        print("  ")
-        print("~~~~check working directory for Zoom_network.png~~~~")
-        print("  ")
-    comment_network(df)
-
-    #Create pdf
+    #Create and Format pdf
     pdf = fpdf.FPDF(format = 'letter')
-    #Page 1
     pdf.add_page()
-    pdf.set_font("Arial", size = 15)
+        #Header
+    pdf.set_font('Arial', 'B', size = 25)
     pdf.cell(200, 10, txt = "Zoom Chat - Basic Analytics",  
          ln = 1, align = 'C') 
+    
+    #Page 1 - Comments by author
+    pdf.set_font('Arial', 'B', size = 15)
+    pdf.cell(200, 10, txt = "Number of comments by each author", ln = 2) 
+    
     author_list = comments_by_author(df)
-
+    pdf.set_font('Arial', size = 15)
     for author, count in author_list.items():
         pdf.cell(200, 5, txt = f"{author} - {count}", ln = 1)
 
-    #Page 2
+    #Page 2 - URLs
     pdf.add_page()
+    pdf.set_font('Arial', 'B', size = 15)
+    pdf.cell(200, 10, txt = "urls shared", ln=2) 
+    
     url_list = find_urls(df)
+    pdf.set_font('Arial', size = 12)
     for url in url_list: 
-        pdf.cell(200, 5, txt = url, ln = 1)
-    #Page 3
+        pdf.write(5, txt = url + "\n")
+    
+    #Page 3 - Histogram
+    pdf.add_page(orientation = 'L')
+    pdf.set_font('Arial', 'B', size = 15)
+    
+    comments_over_time(df)    
+    pdf.image("Zoom_histogram.png", w=250, h=180)
+    
+    #Page 4 - Network
     pdf.add_page()
-    pdf.cell(200, 10, txt = "Zoom Chat Histogram",  
-         ln = 1, align = 'C')
-    pdf.image("Zoom_histogram.png", w=190, h=120)
-    #Page 4
-    pdf.add_page()
-    pdf.cell(200, 10, txt = "Zoom Chat Network",  
-         ln = 1, align = 'C')
+    pdf.cell(200, 10, txt = "Zoom Chat Network", ln = 1, align = 'C')
+    comment_network(df)
     pdf.image("Zoom_network.png", w=200, h=200)
+    
+        #footer
+    pdf.set_font('Arial', size = 10)
+    pdf.multi_cell(180, 25, txt = "Document created with https://github.com/LVParkinson/BNZ_2020_ZoomNLP ", align = 'C')
 
     pdf.output("test.pdf")
 
-
-
-
-    print("\n~~~~Pipeline complete. Check working directory~~~~\n")        
+    print("\n~~~~start-to-finish complete. Check working directory~~~~\n")        
 
     
 if __name__ == '__main__':
@@ -271,5 +212,6 @@ if __name__ == '__main__':
       'comments_by_author': comments_by_author,
       'comments_over_time': comments_over_time,
       'comment_network': comment_network,
+      'individual_function': individual_function,
       'start_to_finish': start_to_finish
   })
